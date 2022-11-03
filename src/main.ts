79cd845fef5cc9284078167ac031e61a98fd5634
no822 <nolan.kim@cobosys.co.kr>
2022-11-03 16:53:48 +0900

    feature/ 고정된 DOM 요소 마크업 / 스타일 추가
    <고정된 DOM 요소들>
    1. 배경영역과 콘텐츠영역 구분
    2. 콘텐츠 영역 내 헤더/메인/푸터 구분
    3. 헤더 타이틀과 섹션 추가 버튼
    4. 메인영역
    5. 푸터, 푸터 컨텐츠

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+// - must have
+    // - As a user, I want to add an image
+    // - As a user, I want to add YouTube video
+    // - As a user, I want to add a note
+    // - As a user, I want to add Todolist
+    // - As a user, I want to delete sections
+
+// 어플리케이션 흐름
+// class diagram?
+// 주요 actors 생각해보기
+
+
+console.log('test');
+
+
