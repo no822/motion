19ad35cde030ac9587cd87f9da9f3a39e751f31b
no822 <nolan.kim@cobosys.co.kr>
2022-11-10 16:22:40 +0900

    fix: 변수명 수정, 문장의 단락 구분

--- src/dragEvent/drag.ts
+++ src/dragEvent/drag.ts
@@ -1,5 +1,3 @@
-import { SectionType } from "../sections/sectionMaker.js";
-
 export interface Drag {
     dragStartHandler: (e: DragEvent) => void;
     dragEndHandler: (e: DragEvent) => void;
@@ -33,32 +31,35 @@ export class DragHandler implements Drag {
 
     private isAppendWrongContainer = (childContainer: HTMLElement): boolean => {
         const sections = document.querySelectorAll('.section');
-        const isWrongAppend: boolean = Array.from(sections).map(section => section.contains(childContainer)).some(isChild => isChild);
-        return isWrongAppend;
+        return Array.from(sections)
+                .map(section => section.contains(childContainer))
+                .some(isChild => isChild);
     }
 
     private getAfterElement = (container: HTMLElement, elementId: string, targetY: number): HTMLElement | null => {
-        const listExceptTarget = ([...container.querySelectorAll(`.section:not(#${elementId})`)]) as Array<HTMLElement>;
-        const afterElement = listExceptTarget
-                .filter(section => {
-                    const { height, y } = section.getBoundingClientRect();
-                    const offsetY = targetY - (y + (height / 2));
-                    return offsetY < 0;
-                })[0];
-        return afterElement;
+        const elementsExceptTarget = ([...container.querySelectorAll(`.section:not(#${elementId})`)]) as Array<HTMLElement>;
+        const afterElements = elementsExceptTarget.filter(section => {
+            const { height, y } = section.getBoundingClientRect();
+            const offsetY = targetY - (y + (height / 2));
+            return offsetY < 0;
+        });
+        return afterElements[0];
     }
 
     dropHandler = (e: DragEvent) => {
         if (e.dataTransfer != null && e.target instanceof HTMLElement) {
             const container = e.target;
             if (this.isAppendWrongContainer(container)) return;
+
+            e.dataTransfer.dropEffect = "move";
             const elementId = e.dataTransfer.getData(this.mimeType);
             const movedElement = document.getElementById(elementId) as HTMLElement;
             const afterElement = this.getAfterElement(container, elementId, e.y);
-            e.dataTransfer.dropEffect = "move";
+
             afterElement
                 ? container.insertBefore(movedElement, afterElement)
                 : container.append(movedElement);
+
             this.refreshList(); // 정렬뒤 인덱스 재정렬
         }
     }
